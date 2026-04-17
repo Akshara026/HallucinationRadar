b33c6b41ed22e630b08076206bf1309960ff21d8
Akshara <kaise9900@gmail.com>
2026-04-17 22:18:30 +0530

    Update testingLLMe.py

--- testingLLM/testingLLMe.py
+++ testingLLM/testingLLMe.py
@@ -4,7 +4,6 @@
 # from langchain_ollama import ChatOllama
 
 
-
 # @tool
 # def multiply(a: int, b: int) -> int:
 #     """Multiply a and b."""
@@ -26,13 +25,10 @@
 # tools = [add, multiply, divide]
 
 
-
-
 # llm = ChatOllama(
 #     model="llama3",
 #     temperature=0
 # )
-
 
 
 # prompt = ChatPromptTemplate.from_template("""
@@ -61,7 +57,6 @@
 # """)
 
 
-
 # agent = create_react_agent(llm, tools, prompt)
 
 # agent_executor = AgentExecutor(
@@ -71,19 +66,17 @@
 # )
 
 
-
 # response = agent_executor.invoke({
 #     "input": "What is (10 + 5) * 2?"
 # })
 
 # print("\nFinal Answer:", response["output"])
 
-from langchain.tools import StructuredTool
 from langchain.agents import AgentExecutor, create_tool_calling_agent
+from langchain.tools import StructuredTool
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama import ChatOllama
 from pydantic import BaseModel
-
 
 
 class MathInput(BaseModel):
@@ -94,58 +87,47 @@ class MathInput(BaseModel):
 def add_func(a: int, b: int) -> int:
     return a + b
 
+
 def multiply_func(a: int, b: int) -> int:
     return a * b
+
 
 def divide_func(a: int, b: int) -> float:
     return a / b
 
 
 add = StructuredTool.from_function(
-    func=add_func,
-    args_schema=MathInput,
-    description="Add two numbers"
+    func=add_func, args_schema=MathInput, description="Add two numbers"
 )
 
 multiply = StructuredTool.from_function(
-    func=multiply_func,
-    args_schema=MathInput,
-    description="Multiply two numbers"
+    func=multiply_func, args_schema=MathInput, description="Multiply two numbers"
 )
 
 divide = StructuredTool.from_function(
-    func=divide_func,
-    args_schema=MathInput,
-    description="Divide two numbers"
+    func=divide_func, args_schema=MathInput, description="Divide two numbers"
 )
 
 tools = [add, multiply, divide]
 
 
-llm = ChatOllama(
-    model="llama3.1",
-    temperature=0
+llm = ChatOllama(model="llama3.1", temperature=0)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a strict math assistant. Always use tools."),
+        ("human", "{input}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
 )
-
-
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are a strict math assistant. Always use tools."),
-    ("human", "{input}"),
-    ("placeholder", "{agent_scratchpad}")
-])
 
 
 agent = create_tool_calling_agent(llm, tools, prompt)
 
-agent_executor = AgentExecutor(
-    agent=agent,
-    tools=tools,
-    verbose=True
-)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
 
-response = agent_executor.invoke({
-    "input": "What is (10 + 5) * 2?"
-})
+response = agent_executor.invoke({"input": "What is (10 + 5) * 2?"})
 
 print("\nFinal Answer:", response["output"])
